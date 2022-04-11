1edb2e8c7fde3603f929809126f07a95d5b6bcfd
Nick Nisi <nick@nisi.org>
2022-04-11 09:35:27 -0500

    add basic game machine states

--- src/machines/gameMachine.ts
+++ src/machines/gameMachine.ts
@@ -6,6 +6,20 @@ export type GameMachineEvent = { type: 'CLICK' };
 
 export const gameMachine = createMachine<GameMachineContext, GameMachineEvent>({
   id: 'gameMachine',
+  initial: 'initial',
+  states: {
+    initial: {},
+    idle: {},
+    qustion: {
+      initial: 'default',
+      states: {
+        default: {},
+        showAnswer: {},
+      },
+    },
+    contestants: {},
+    winner: {},
+  },
 });
 
 export default gameMachine;
