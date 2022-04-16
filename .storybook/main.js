bb50d30c692e104458f85fb49767009f32a43ac0
Nick Nisi <nick@nisi.org>
2022-04-16 07:17:37 -0500

    get storybook working with vite

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,20 @@
+const { mergeConfig } = require('vite');
+
 module.exports = {
+  async viteFinal(config, { configType }) {
+    // return the customized config
+    return mergeConfig(config, {
+      // customize the Vite config here
+      resolve: {},
+      define: {
+        ...config.define,
+        global: 'window',
+      },
+    });
+  },
+  core: { builder: '@storybook/builder-vite' },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: '@storybook/react',
-  staticDirs: ['../assets'],
+  staticDirs: ['../public'],
 };
